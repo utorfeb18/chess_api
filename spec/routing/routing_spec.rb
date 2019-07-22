32f6c89968e9927be00964145c336da518534418
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-07-22 16:59:58 -0400

    Renames routes test directory to routing and ping_spec.rb to routing_spec.rb

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Ping route", :type => :routing do
+RSpec.describe "Api routing", :type => :routing do
 
-  it "routes ping requests to api#ping" do
+  it "routing ping requests to api#ping" do
     expect(:get => "/api/ping").to route_to(
         :controller => "api",
         :action => "ping"
       )
   end
 
-  it "does not route an update action" do
+  it "does not route an update action for ping" do
     expect(post: "/api/ping").to_not be_routable
   end
 
