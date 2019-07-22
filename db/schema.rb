84a5daaa0a684961022e9f356ff9503c1b467968
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-07-22 13:30:13 -0400

    Adds game_id column to boards table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_170643) do
+ActiveRecord::Schema.define(version: 2019_07_22_172844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_19_170643) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "game_id"
     t.index ["id", "piece_id"], name: "index_boards_on_id_and_piece_id", unique: true
   end
 
