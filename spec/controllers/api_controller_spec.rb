1cf2fcb1b73cb1776b7c687934942a82c3b70cea
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-07-22 17:28:22 -0400

    Adds test for ping controller action

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
 
+  describe 'api/ping action' do
+
+    it 'should return success in the body' do
+      get :ping
+      expect(response.body).to eq({ "success": true }.to_json)
+    end
+
+    it 'should return 200 status-code' do
+      get :ping
+      expect(response.status).to eq(200)
+    end
+  end
+
 end
